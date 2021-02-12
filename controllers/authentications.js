f63f7af8e654e002a782597fe49d82151648b1db
rowjay007 <rowlives@gmail.com>
2021-02-12 08:37:36 +0100

    install bcrypt-node to hash password

--- controllers/authentications.js
+++ controllers/authentications.js
@@ -3,6 +3,12 @@ exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You most provide email and password" });
+  }
+
   User.findOne({ email: email }, function (err, existingUser) {
     if (err) {
       return next(err);
