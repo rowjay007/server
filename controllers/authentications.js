acb5f4d14ba9918bba34dedb3abc041c41b718ce
rowjay007 <rowlives@gmail.com>
2021-02-15 11:42:56 +0100

    create config file and jwt-simple authentication

--- controllers/authentications.js
+++ controllers/authentications.js
@@ -1,4 +1,12 @@
 const User = require("../models/user");
+const jwt = require("jwt-simple");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -24,7 +32,7 @@ exports.signup = function (req, res, next) {
       if (err) {
         return next(err);
       }
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
