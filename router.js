2f9c01150534ab972246c45e48816480fc3b31f8
rowjay007 <rowlives@gmail.com>
2021-02-15 12:50:50 +0100

    create signin authentication

--- router.js
+++ router.js
@@ -2,10 +2,12 @@ const passportServicer = require("./services/passport");
 const passport = require("passport");
 const Authentication = require("./controllers/authentications");
 
+const requireSignin = passport.authenticate("local", { session: false });
 const requireAuth = passport.authenticate("jwt", { session: false });
 module.exports = function (app) {
   app.get("/", requireAuth, function (req, res) {
     res.send({ hi: "there" });
   });
+  app.post("/signin", requireSignin, Authentication.signin);
   app.post("/signup", Authentication.signup);
 };
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,25 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+//create local strategy
+const localOptions = { usernameFiled: "email" };
+const localLogin = new LocalStrategy(
+  localOptions,
+  function (email, password, done) {
+    //Verify user name and password
+
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+    });
+  }
+);
 
 const jwOptions = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
@@ -20,9 +39,22 @@ const jwtLogin = new JwtStrategy(jwOptions, function (payload, done) {
     } else {
       done(null, false);
     }
+
+    //compare user password
+    user.comparePassword(password, function (err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
   });
 });
 
 //Tell passport to use this strategy
 
 passport.use(jwtLogin);
+passport.use(localLogin);
